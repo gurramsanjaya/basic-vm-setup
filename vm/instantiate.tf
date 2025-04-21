5783554b6f03b558eea7d73fbd0e0f80573f5481
Gurram, Sanjaya <gurramsanjaya@gr1dl0ck.com>
2025-04-21 19:25:39 +0530

    cloud-init: Use terraform provider to render mime type cloud init config Added README.md Parameterized ssh_key templating into the cloud init config Moved the curl and file operations to boothook Moved the services setup to the final posthook

--- vm/instantiate.tf
+++ vm/instantiate.tf
@@ -9,19 +9,46 @@ terraform {
       source = "hashicorp/random"
       version = "3.7.1"
     }
+    cloudinit = {
+      source = "hashicorp/cloudinit"
+      version = "2.3.6"
+    }
   }
 }
 
-variable "do_token" {}
-variable "name_suffix" {}
-variable "disable_ssh" {
-  type = bool
+variable "do_token" {
+  nullable = false
+  sensitive = true
+}
+variable "name_suffix" {
+  nullable = false
 }
 variable "ssh_public_key_file" {
+  nullable = false
   default = "../vars/ssh_key.pem.pub"
 }
-variable "user_data_file" {
-  default = "../vars/user-script-file.yaml"
+variable "use_cloudinit" {
+  type = bool
+}
+variable "cloud_config_base_template" {
+  default = "../vars/cloud-config.tftpl"
+}
+variable "cloud_config_extra" {
+  type = map(object({
+    content_type = string
+    file = string
+  }))
+  default = {
+    "cloud-boothook.sh" = {
+      content_type = "text/cloud-boothook"
+      file = "../vars/cloud-boothook.sh"
+    },
+    "cloud-posthook.sh" = {
+      content_type = "text/x-shellscript"
+      file = "../vars/cloud-posthook.sh"
+    }
+  }
+  nullable = true
 }
 
 provider "digitalocean" {
@@ -39,15 +66,37 @@ data "digitalocean_regions" "doreg" {
     }
 }
 
-resource "random_integer" "random" {
-    min = 0
-    max = length(data.digitalocean_regions.doreg.regions) - 1
+# Should output MIME multi-part archive not compressed with gzip, cloud-init throws error on base64 encoded mime content
+data "cloudinit_config" "c_config" {
+  count = var.use_cloudinit ? 1 : 0
+  # Can't base64 encode the mime multipart config - https://github.com/canonical/cloud-init/issues/4239
+  base64_encode = false
+  gzip = false
+  part {
+    content_type = "text/cloud-config"
+    content = fileexists(var.cloud_config_base_template) ? templatefile(var.cloud_config_base_template, { ssh_key : file(var.ssh_public_key_file) }) : null
+    filename = "cloud_config_base"
+  }
+  dynamic part {
+    for_each = var.cloud_config_extra
+    content {
+      content_type = part.value["content_type"]
+      content = fileexists(part.value["file"]) ? file(part.value["file"]) : null
+      filename = part.key
+    }
+  }
 }
 
 resource "digitalocean_ssh_key" "ssh_key" {
-    count = var.disable_ssh ? 0 : 1
+    count = var.use_cloudinit ? 0 : 1
     name = "ssh-${var.name_suffix}"
     public_key = file(var.ssh_public_key_file)
+}
+
+# For randomizing the region
+resource "random_integer" "random" {
+    min = 0
+    max = length(data.digitalocean_regions.doreg.regions) - 1
 }
 
 resource "digitalocean_droplet" "web" {
@@ -58,7 +107,7 @@ resource "digitalocean_droplet" "web" {
     ipv6 = true
     ssh_keys = digitalocean_ssh_key.ssh_key[*].id
     resize_disk = false
-    user_data = fileexists(var.user_data_file) ? file(var.user_data_file) : null
+    user_data = one(data.cloudinit_config.c_config[*].rendered)
 }
 
 output "ipv4_address" {
--- vm/terraform.tfvars
+++ vm/terraform.tfvars
@@ -1,1 +1,2 @@
-disable_ssh=true
+use_cloudinit = true
+ssh_public_key_file = "../vars/test.pem.pub"
