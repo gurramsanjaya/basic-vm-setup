79c17fa7a45988ec8375cdc9068bfab6c3267415
Gurram, Sanjaya <gurramsanjaya@gr1dl0ck.com>
2026-01-25 03:09:49 +0530

    refactor(dnsblocklist): sort and remove comments in file generation
    Signed-off-by: Gurram, Sanjaya <gurramsanjaya@gr1dl0ck.com>

--- dnsblocklist/generate_blocklists.sh
+++ dnsblocklist/generate_blocklists.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 # TODO: make a github flow that autoupdates this or something....
 
@@ -10,6 +10,15 @@ set -e
 : ${FORCE_ALLOW:=true}
 : ${FORCE_ALLOW_FILE:=force_allow}
 
+
+RESULTS_TEMP="${RESULTS_FILE}_temp"
+
+function cleanup() {
+    echo "cleaning up..."
+    rm -f "$RESULTS_TEMP"
+    trap - 0
+}
+trap "cleanup" 0 SIGHUP SIGTERM SIGINT
 
 # just to make sure the first entry of the next file/download doesn't get directly appended
 # to the last entry of the previous download
@@ -23,19 +32,19 @@ AWK_PRG='/^[^#][^[:space:]]*$/'
 
 mapfile -t URLS < <(awk "$AWK_PRG" "$DENY_URLS_FILE" )
 
-> "$RESULTS_FILE"
+> "$RESULTS_TEMP"
 for u in "${URLS[@]}"; do
   echo "Trying $u ..."
-  # echo "##  DenyList from ${RESULTS_FILE}" >> "$RESULTS_FILE"
-  curl -sSL "$u" >> "$RESULTS_FILE"
-  ensure_spacing "$RESULTS_FILE"
+  # echo "##  DenyList from ${RESULTS_TEMP}" >> "$RESULTS_TEMP"
+  curl -sSL "$u" >> "$RESULTS_TEMP"
+  ensure_spacing "$RESULTS_TEMP"
 done
 
 if [[ $DENY_EXTRAS == "true" ]]; then
   echo "Adding extra denylist from ${DENY_EXTRAS_FILE}"
-  # echo "## Extras from ${DENY_EXTRAS_FILE}" >> "$RESULTS_FILE"
-  cat "$DENY_EXTRAS_FILE" >> "$RESULTS_FILE"
-  ensure_spacing "$RESULTS_FILE"
+  # echo "## Extras from ${DENY_EXTRAS_FILE}" >> "$RESULTS_TEMP"
+  cat "$DENY_EXTRAS_FILE" >> "$RESULTS_TEMP"
+  ensure_spacing "$RESULTS_TEMP"
 fi
 
 ## force allow, might not work correctly...
@@ -43,6 +52,14 @@ fi
 if [[ $FORCE_ALLOW == "true" ]]; then
   while IFS= read -r line; do
     echo "Force allowing ${line} ..."
-    sed -i '/^[^[:space:]]*'"$line"'[^[:space:]]*$/d' "$RESULTS_FILE"
+    sed -i '/^[^[:space:]]*'"$line"'[^[:space:]]*$/d' "$RESULTS_TEMP"
   done < <(awk "$AWK_PRG" "$FORCE_ALLOW_FILE")
 fi
+
+## remove any comments
+echo "Removing any comments..."
+sed -i '/^[[:space:]]*#/d' "$RESULTS_TEMP"
+
+## finally sort it
+echo "Sorting the file..."
+sort "$RESULTS_TEMP" > "$RESULTS_FILE"
