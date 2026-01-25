7bd79bd792e3d25dbc2847032a78085877bcdf0a
Gurram, Sanjaya <gurramsanjaya@gr1dl0ck.com>
2026-01-26 02:10:48 +0530

    chore(vm-setup): decorate the trigger script a bit
    Signed-off-by: Gurram, Sanjaya <gurramsanjaya@gr1dl0ck.com>

--- vm-setup.sh
+++ vm-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 : ${PORT:=59995}
 : ${PLAN:=outplan}
@@ -10,6 +10,26 @@ set -e
 : ${SKIP_WGE_CLIENT:=false}
 : ${PRE_SLEEP_WGE_CLIENT:=150}
 # set DN_OVERRIDE to override the DN value that will by default pull from openssl.cnf
+
+## really unnecessary, but whatever
+function banner() {
+  echo '''
+                                       __
+ _   __ ____ ___          _____ ___   / /_ __  __ ____
+| | / // __ `__ \ ______ / ___// _ \ / __// / / // __ \
+| |/ // / / / / //_____/(__  )/  __// /_ / /_/ // /_/ /
+|___//_/ /_/ /_/       /____/ \___/ \__/ \__,_// .___/
+                                              /_/
+
+'''
+}
+
+function hrule() {
+  printf '\n%*s\n' "$(tput cols)" | sed 's/ /\xe2\x80\x95/g'
+  if [ ! -z ${1+x} ]; then
+    echo -e "$(tput bold)${1}: \n$(tput sgr0)"
+  fi
+}
 
 
 # this tries matching '[server_extensions]' and then looks for the first 'subjectAltName = DNS:<domain>' along the next lines
@@ -29,6 +49,9 @@ BEGIN { i=0 }
   }
 }
 '
+
+banner
+
 if [ -z ${DN_OVERRIDE+x} ]; then
   DN=$(awk "$AWK_PRG" openssl.cnf)
 else
@@ -38,7 +61,7 @@ echo "using domain name: ${DN}"
 
 
 # basic checks
-echo -e "\n=======Basic Checks=======\n"
+hrule "Basic Checks"
 wge-client -version
 jq --version
 "$TERRAFORM_CLIENT" -version
@@ -51,7 +74,7 @@ mkdir -p "$CLIENT_DIR"
 
 pushd vm 1>/dev/null
 if [[ $SKIP_CREATE != "true" ]]; then
-  echo -e "\n=======Instance Create=====\n"
+  hrule "Instance Create"
   "$TERRAFORM_CLIENT" plan -out ${PLAN}
   "$TERRAFORM_CLIENT" apply ${PLAN}
 fi
@@ -60,14 +83,20 @@ fi
 if [[ $SKIP_WGE_CLIENT == "true" ]]; then
   popd 1>/dev/null
 else
-  echo -e "\n======/etc/hosts Update====\n"
-  INSTANCE_IP=$("$TERRAFORM_CLIENT" output -json | jq -r .ipv4_address.value)
-  echo "using ip retrieved from state: ${INSTANCE_IP}"
-  popd 1>/dev/null
-
   if [[ $SKIP_HOSTS_MOD != "true" ]]; then
+    hrule "/etc/hosts Update"
+    INSTANCE_IP=$("$TERRAFORM_CLIENT" output -json | jq -r .ipv4_address.value)
+    echo "using ip retrieved from terraform state: ${INSTANCE_IP}"
+    popd 1>/dev/null
+
     ## careful here, we are messing around with the /etc/hosts
-    echo "replacing domain for ${INSTANCE_IP} if any and saving it with ${DN} and replacing ip for ${DN} if any and replacing it with ${INSTANCE_IP}"
+    echo "removing domain for ${INSTANCE_IP} if any, removing ip for ${DN} if any, creating a new 1-1 entry with ${INSTANCE_IP} - ${DN}"
+    read -p "Continue (y/n)? " CONTINUE
+    if ! [[ $CONTINUE =~ ^[[:space:]]*[yY] ]]; then
+      echo "quiting..."
+      exit 1
+    fi
+
     sudo -s -- <<EOF
 cp /etc/hosts /etc/hosts_old
 sed -i '/.*${INSTANCE_IP}.*/d' /etc/hosts
@@ -77,11 +106,11 @@ printf '\n%s\t%s' $INSTANCE_IP "$DN" >> /etc/hosts
 EOF
   fi
 
-  echo -e "\n===Nslookup Verification===\n"
+  hrule "Nslookup Verification"
   nslookup "$DN"
   read -p "press enter to continue..."
 
-  echo -e "\n======Starting client======\n"
+  hrule "Starting WG client"
   CWD=$(pwd)
   pushd "$CLIENT_DIR" 1>/dev/null
   if [[ $SKIP_CREATE != "true" ]]; then
